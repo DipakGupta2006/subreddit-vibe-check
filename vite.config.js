c5fa93e114f856c658787f05bce9346773583aef
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-08-19 19:42:03 +0530

    fix: CORS proxy added

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
+}
